0de68721c706f6dbc5dc1a7ce7946c807d0e0d04
Nick Bray <ncbray@google.com>
2019-05-21 13:20:40 -0700

    [libstdc++-trusty] Switch to useing a subset of libcxx
    Also add support for smart pointers.
    
    Bug: 110158907
    Change-Id: I9b41e822f2c7b19fe7bbbc06349aff5a003b04fb

--- lib/libstdc++-trusty/rules.mk
+++ lib/libstdc++-trusty/rules.mk
@@ -2,9 +2,16 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
 
-GLOBAL_INCLUDES += $(LOCAL_DIR)/include
+LIBCXX_DIR = external/libcxx
+
+GLOBAL_INCLUDES += $(LIBCXX_DIR)/include
+
+# The header files change if they're being used to build the library.
+# For example, adding "public" methods that are only used internally.
+MODULE_COMPILEFLAGS += -D_LIBCPP_BUILDING_LIBRARY
 
 MODULE_SRCS := \
-	$(LOCAL_DIR)/new.cpp \
+        $(LIBCXX_DIR)/src/memory.cpp \
+        $(LIBCXX_DIR)/src/new.cpp \
 
 include make/module.mk
