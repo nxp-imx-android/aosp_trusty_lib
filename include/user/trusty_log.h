821601939b9662d6374acec69e17ca55d283529a
Ji Luo <ji.luo@nxp.com>
2020-03-19 16:08:29 +0800

    MA-16713-3 Fix trusty build errors
    Fix build errors when add the sample tests.
    
    Test: builds.
    
    Change-Id: I241ab3ccd38fdba0d92882362a0a7c4c397bae97
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- include/user/trusty_log.h
+++ include/user/trusty_log.h
@@ -68,11 +68,11 @@ __END_CDECLS
     } while (0)
 
 /* info */
-#define TLOGI(x...)                      \
+#define TLOGI(x, ...)                      \
     do {                                 \
         if (TLOG_LVL >= TLOG_LVL_INFO) { \
-            fprintf(stderr, "%s: " x, TLOG_TAG); \
-            unittest_printf("%s: " x, TLOG_TAG); \
+            fprintf(stderr, "%s: " x, TLOG_TAG, ##__VA_ARGS__); \
+            unittest_printf("%s: " x, TLOG_TAG, ##__VA_ARGS__); \
         }                                \
     } while (0)
 
