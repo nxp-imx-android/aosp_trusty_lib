5c72e8294f4b6ae25755599abd144991c939589b
Sagar Shah <sagshah@google.com>
2020-06-25 15:00:56 -0700

    Move libcxx-test from sample apps to /user/base/lib/libstdc++-trusty directory.
    Bug: 159488974
    Change-Id: I100f79a22a14a8bb751b61d72d63414931abb0da

--- lib/libstdc++-trusty/test/rules.mk
+++ lib/libstdc++-trusty/test/rules.mk
@@ -2,25 +2,32 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# This file lists userspace tests
+LOCAL_DIR := $(GET_LOCAL_DIR)
 
-[
-    include("trusty/user/app/keymaster/build-config-usertests"),
-    include("trusty/user/app/sample/build-config-usertests"),
+MODULE := $(LOCAL_DIR)
 
-    porttest("com.android.ipc-unittest.ctrl"),
-    porttest("com.android.libctest"),
-    porttest("com.android.libcxxtest"),
-    porttest("com.android.trusty.smc.test"),
-    porttest("com.android.uirq-unittest"),
-]
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/libcxx_test.cpp \
+
+MODULE_DEPS += \
+	trusty/user/base/lib/libc-trusty \
+	trusty/user/base/lib/libstdc++-trusty \
+	trusty/user/base/lib/unittest \
+
+# Don't let the compiler optimize out the code we're testing. new, delete, etc.
+MODULE_COMPILEFLAGS := -ffreestanding
+
+include make/module.mk
--- usertests-inc.mk
+++ usertests-inc.mk
@@ -19,6 +19,7 @@ include trusty/user/app/storage/usertests-inc.mk
 
 TRUSTY_ALL_USER_TASKS += \
         trusty/user/base/lib/libc-trusty/test \
+        trusty/user/base/lib/libstdc++-trusty/test \
 	trusty/user/base/lib/smc/tests \
 	trusty/user/base/lib/tipc/test/main \
 	trusty/user/base/lib/tipc/test/srv \
