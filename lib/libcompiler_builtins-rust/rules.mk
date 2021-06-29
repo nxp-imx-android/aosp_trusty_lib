92380cad89f15f19ef4cda0b89c6b086079900ac
Stephen Crane <cranes@google.com>
2021-06-28 19:28:25 -0700

    [rust] Add compiler_builtins to Trusty
    Add the compiler-builtins crate to the Trusty build system. This crate
    is needed to build for a target which doesn't have a prebuilt libstd.
    
    Test: build.py
    Bug: 192309116
    Change-Id: I738e637e4015a789944d52ad0adcda4f6d21bf73

---
+++ lib/libcompiler_builtins-rust/rules.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+COMPILER_BUILTINS_DIR = $(RUST_BINDIR)/../src/stdlibs/vendor/compiler_builtins
+
+MODULE_SRCS := $(COMPILER_BUILTINS_DIR)/src/lib.rs
+
+MODULE_CRATE_NAME := compiler_builtins
+
+MODULE_LIBRARY_DEPS += \
+	trusty/user/base/lib/libcore-rust \
+
+MODULE_RUSTFLAGS += \
+	--cfg 'feature="compiler-builtins"' \
+	--cfg 'feature="core"' \
+	--cfg 'feature="default"' \
+	--edition 2015 \
+	-C panic=abort \
+	-C overflow-checks=off \
+
+MODULE_ADD_IMPLICIT_DEPS := false
+
+include make/library.mk
