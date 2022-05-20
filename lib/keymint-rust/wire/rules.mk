8a4d69fcf7313b6e11c2f1b88aa1960672784922
Hasini Gunasinghe <hasinitg@google.com>
2022-05-20 01:32:54 +0000

    Add build files for the Rust crates of Keymint reference implementation.
    Bug: 232784532
    Test: Build Keymint TA with no build errors.
    Change-Id: I2bc33773c193262347e9d9f966f212ed85979533

---
+++ lib/keymint-rust/wire/rules.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := system/keymint/wire/src/lib.rs
+
+MODULE_CRATE_NAME := kmr_wire
+
+MODULE_LIBRARY_DEPS += \
+    trusty/user/base/host/enumn-rust \
+	trusty/user/base/host/keymint-rust/derive \
+	trusty/user/base/lib/ciborium-io-rust \
+	trusty/user/base/lib/ciborium-rust \
+	trusty/user/base/lib/coset-rust \
+	trusty/user/base/lib/log-rust \
+	trusty/user/base/lib/zeroize-rust \
+
+include make/library.mk
