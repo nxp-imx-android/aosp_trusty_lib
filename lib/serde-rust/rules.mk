09d2bea10f1676438aab6e47533e188bbc2eea64
Hasini Gunasinghe <hasinitg@google.com>
2022-05-20 01:24:47 +0000

    Add rules.mk files for the external rust crates for Keymint-Rust.
    Bug: 232342562
    Test: verify successful build
    Change-Id: I989616965858e80406b9bb5c5ed8c41ce46cb361

---
+++ lib/serde-rust/rules.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := external/rust/crates/serde/src/lib.rs
+
+MODULE_CRATE_NAME := serde
+
+MODULE_RUST_EDITION := 2015
+
+MODULE_RUSTFLAGS += \
+	--cfg 'feature="default"' \
+	--cfg 'feature="alloc"' \
+	--cfg 'feature="derive"' \
+	--cfg 'feature="serde_derive"' \
+
+MODULE_LIBRARY_DEPS += \
+	trusty/user/base/lib/liballoc-rust \
+	trusty/user/base/host/serde_derive-rust \
+
+include make/library.mk
