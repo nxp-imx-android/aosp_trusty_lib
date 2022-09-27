bf7041150e1f035cd45730a8c7ee14fce07fb515
Hasini Gunasinghe <hasinitg@google.com>
2022-09-27 00:03:39 +0000

    Add Trusty build files for all the new crates required for cert gen.
    Bug: 239549209
    Bug: 239476605
    Change-Id: I0325cba9f58c79e3a7de0cc851dbaedff8711bf6

---
+++ host/synstructure/rules.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := external/rust/crates/synstructure/src/lib.rs
+
+MODULE_CRATE_NAME := synstructure
+
+MODULE_RUST_EDITION := 2018
+
+MODULE_RUSTFLAGS += \
+	--cfg 'feature="proc-macro"' \
+
+MODULE_LIBRARY_DEPS += \
+	trusty/user/base/host/proc-macro2 \
+	trusty/user/base/host/proc-macro-error \
+	trusty/user/base/host/quote \
+	trusty/user/base/host/syn \
+
+include make/library.mk
