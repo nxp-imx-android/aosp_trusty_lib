96414c22e1bcdf03471c6987b298b28cb4eb5160
Mike McTernan <mikemcternan@google.com>
2023-06-02 17:27:05 +0100

    trusty:memref: fix incorrect return value
    Test: build.py --test com.android.memref.test
    Bug: 285006560
    Change-Id: Ibe8ea63ad26c14e07c1eacfb6ceef8f7b6b2444e

--- lib/libc-trusty/memref.c
+++ lib/libc-trusty/memref.c
@@ -13,7 +13,7 @@ int memref_create(void* addr, size_t size, uint32_t mmap_prot) {
 #endif
     /* Check size fits in a uint32_t until size_t in syscalls is supported */
     if (size > UINT32_MAX) {
-        return -ERR_INVALID_ARGS;
+        return ERR_INVALID_ARGS;
     }
     return _trusty_memref_create(addr, size, mmap_prot);
 }
