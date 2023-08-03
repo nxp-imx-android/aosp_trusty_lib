8ccea0b22d35e7af2b8fa036aff38741f1d795b1
David Benjamin <davidben@google.com>
2023-08-03 22:09:43 +0000

    Remove openssl-stubs/rand.c and trusty_rng_internal_system_rand
    As of aosp/2689386, BoringSSL internally knows how to call
    Trusty's APIs, so Trusty no longer needs to implement CRYPTO_sysrand.
    Moreover, this leaves trusty_rng_internal_system_rand unused. Remove
    both.
    
    The history here is that Trusty used to point BoringSSL to Trusty's
    entropy pool. Then aosp/1777305 made CRYPTO_sysrand_for_seed call the
    underlying hardware RNG directly, while plain CRYPTO_sysrand stayed on
    Trusty's entropy pool for performance since it was called on every
    RAND_bytes call.
    
    These frequent calls in BoringSSL are for fork-safety, on more typical
    platforms, where the application may fork and where getting entropy is
    cheap. But as Trusty doesn't fork, this doesn't matter. And even if
    Trusty did fork, CRYPTO_sysrand implemented with an in-memory entropy
    pool means we're not protecting against forks anyway.
    
    So newer BoringSSLs (imported to AOSP in aosp/2671600) know not to
    bother for Trusty. That means there are no longer perf requirements
    between CRYPTO_sysrand and CRYPTO_sysrand_for_seed, so BoringSSL's
    built-in Trusty support doesn't use trusty_rng_internal_system_rand.
    Thus we can remove that.
    
    This also means trusty_rng_add_entropy is no longer needed, as it
    updates an entropy pool that no one reads. But it has calls throughout
    Trusty, so I'm leaving it as a no-op. The corresponding BoringSSL call
    would be RAND_seed, but RAND_seed in BoringSSL does not consume the
    input either. BoringSSL expects the platform to provide a healthy
    entropy source.
    
    Bug: None
    Test: treehugger
    Change-Id: If7505bff03b5cfec4cd0d05afc41f7141e7804b2

--- lib/openssl-stubs/openssl-stubs-inc.mk
+++ lib/openssl-stubs/openssl-stubs-inc.mk
@@ -1,9 +1,4 @@
 # TODO: Move Trusty boringssl build into this repo, like we do for musl
 
-OPENSSL_STUBS_DIR := $(GET_LOCAL_DIR)
-
-MODULE_SRCS += \
-	$(OPENSSL_STUBS_DIR)/rand.c \
-
 MODULE_LIBRARY_DEPS += \
 	trusty/user/base/lib/rng \
